03f915d2bd73b02a151966f036f8e392542f4d5c
victor ortega <vorteg.r@gmail.com>
2023-06-19 16:06:50 -0600

    Full aplication

---
+++ app/api/prompt/route.js
@@ -0,0 +1,19 @@
+import { connectToDB } from '@/utils/database';
+import Prompt from '@/models/prompt';
+
+export const GET = async (request) => {
+    try {
+        await connectToDB()
+
+        const prompts = await Prompt.find({}).populate('creator')
+
+        return new Response(JSON.stringify(prompts), {
+            status:200
+        })
+    }catch(error){
+        console.log(error)
+        return new Response('Faild to fetch all prompts', {
+            status:500
+        })
+    }
+}
---
+++ app/layout.jsx
@@ -0,0 +1,34 @@
+import '@/styles/global.css'
+
+import Nav from '@/components/Nav'
+import Provider from '@/components/Provider'
+
+export const metadata = {
+  title: "PromtsAI",
+  description: "Discovery & Share AI Prompts"
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+
+      <body>
+        <Provider>
+          <div className='main'>
+            <div className='gradient'>
+
+            </div>
+
+          </div>
+          <main className='app'>
+            <Nav />
+            {children}
+
+          </main>
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
